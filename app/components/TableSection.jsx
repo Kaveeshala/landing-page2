50b3522903bb431fee7fabb480546be717765991
kaveeshala <it22110916@my.sliit.lk>
2025-03-12 16:02:23 +0530

    modify the changes in table section

--- app/components/TableSection.jsx
+++ app/components/TableSection.jsx
@@ -3,28 +3,48 @@ import tableimg from "../../public/images/tableimg.png";
 
 export default function TableSection() {
   return (
-    <div className="flex border border-gray-400">
+    <div className="flex border border-gray-300 mt-30">
      
-      <div className="flex-shrink-0 w-1/12 border-r border-gray-400">
+      <div className="flex-shrink-0 w-1/24 border-r border-gray-300">
       
       </div>
 
+      <div className="flex-1 border-r border-gray-300 px-6">
+        <h1 className=" text-2xl mt-6">Consultation</h1>
+        <p className=" text-sm  text-gray-400 leading-tight mt-4">Our Consultation provides expert
+           guidance on tailored solar solutions,
+           helping you make informed decisions for
+           a sustainable energy future</p>
+
+           <h1 className="underline text-sm cursor-pointer mt-6">LEARN MORE</h1>
     
-      <div className="flex-1 border-r border-gray-400">
-      
       </div>
 
      
-      <div className="flex-1 border-r border-gray-400">
+      <div className="flex-1 border-r border-gray-300 px-6">
+        <h1 className="text-2xl mt-67">Survay</h1>
+        <p className=" text-sm  text-gray-400 leading-tight mt-4">Our Survey Process evaluates your site
+           and energy needs to design an optimal
+           solar solution</p>
+
+           <h1 className="text-sm underline cursor-pointer mt-6">LEARN MORE</h1>
       </div>
 
-      <div className="flex-1 border-r border-gray-400">
+      <div className="flex-1 border-r border-gray-300 px-6">
+        <h1 className="text-2xl mt-113">Execute</h1>
+        <p className=" text-sm  text-gray-400 leading-tight mt-4">Our Execution Process ensures seamless
+           installation and integration of your solar
+           system, delivering efficient, reliable, and
+           sustainable energy solutions.</p>
+
+           <h1 className="text-sm underline cursor-pointer mt-6">LEARN MORE</h1>
 
       </div>
-      
+
       <div className="flex-2">
         <Image src={tableimg} alt="tableimg" className="w-full h-full object-cover" />
       </div>
     </div>
+    
   );
 }
